2076f3e5b3cf53e9d7b9533e4bd8c38640276c41
CBolle <crbolle.zakelijk@gmail.com>
2024-07-22 14:11:32 +0200

    setup database connection and implement structure

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 from src.views.view import View
+from src.data_access.database import Database
 
-def main():
-    view = View()
-    view.run()
+class Main():
+    def __init__(self):
+        self.database = Database()
+        self.view = View()
+        self.view.run()
+
 
 if __name__ == "__main__":
-    main()
+    main = Main()
--- src/views/view.py
+++ src/views/view.py
@@ -1,3 +1,5 @@
+from src.data_access.services.animal_service import Animal_Service
+
 class View():
     def __init__(self):
         pass
@@ -6,12 +8,16 @@ class View():
         # Print all options here
         pass
 
+    def add_animal(self):
+       self.service = Animal_Service()
+       self.service.add_animal()
+
     def run(self):
         while True:
             self.display_menu()
             choice = input("Choose an option: ")
             if choice == '1':
-                pass  #add controller commands here such as controller.show_zookeeper()
+                self.add_animal()
             elif choice == '2':
                 pass
             elif choice == '3':

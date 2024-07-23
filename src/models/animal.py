8c762d2c884046164570ac6f7a778cea5659f983
CBolle <crbolle.zakelijk@gmail.com>
2024-07-23 16:28:53 +0200

    deleted unneccesary func

--- src/models/animal.py
+++ src/models/animal.py
@@ -14,10 +14,7 @@ class Animal(Base):
     species = relationship('src.models.species.Species', back_populates='animal')
 
     __table_args__ = {'extend_existing': True}
-
-    def get_life_span(self):
-        return self.species.life_span
-        
+    
     def get_age(self):
         return ((datetime.today() - self.date_of_birth))/365.25
 

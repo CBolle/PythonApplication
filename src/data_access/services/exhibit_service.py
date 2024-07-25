ce36a396b115702bc42d6b3c70e70cf10889007e
CBolle <crbolle.zakelijk@gmail.com>
2024-07-25 12:39:01 +0200

    check enum

--- src/data_access/services/exhibit_service.py
+++ src/data_access/services/exhibit_service.py
@@ -11,20 +11,36 @@ class Exhibit_Service:
         self.repo = ExhibitRepository(database.get_session())
         self.keylist = [[column.name,column.type] for column in inspect(Exhibit).c]
 
+    def get_species(self):
+        
 
     def add(self):
         self.args = {}
 
         for i in range(len(self.keylist)):
-            if self.keylist[i][0] == "id": ##Add any other things if they should be auto-incremeneted
+            if self.keylist[i][0] == "id": #Add any other things if they should be auto-incremeneted
                 continue
 
             keyname = self.keylist[i][0]
             keytype = self.keylist[i][1]
                 
-            for type in typedict().keys(): ##Add types to dictionairies.py if your type is not found
-                if isinstance(keytype, type):
-                    self.input = typedict()[type](input(f'{keyname}: '))
+            for type_i in typedict().keys(): #Add types to dictionairies.py if your type is not found
+                if isinstance(keytype, type_i):
+                    if type(keytype).__name__ == "Enum":
+                        if keyname == 'landscape':
+                            print('Which landscape will your exhibit have? Choose from:')
+                            for name in Landscape.__members__:
+                                print(name)
+                            while True:
+                                testinput = typedict()[type_i](input(f'{keyname}: ')).upper()
+                                if testinput in Landscape.__members__:
+                                    self.input = testinput
+                                    break
+                                else:
+                                    print("Try again")
+                        # add other enum classes in if-statements if necessary
+                    else:
+                        self.input = typedict()[type_i](input(f'{keyname}: '))
                     
             self.args[keyname] = self.input
         

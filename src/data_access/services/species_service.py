a52a51af74e70809cb05e7c3b093172283b2b9f1
CBolle <crbolle.zakelijk@gmail.com>
2024-07-29 14:53:04 +0200

    working update

--- src/data_access/services/species_service.py
+++ src/data_access/services/species_service.py
@@ -1,7 +1,7 @@
 from src.data_access.repositories.species_repo import SpeciesRepository
 from src.models.species import Species
 from src.data_access.database import database
-from src.data_access.dictionaries import typedict
+# from src.data_access.dictionaries import typedict
 from sqlalchemy.inspection import inspect
 
 class Species_Service:
@@ -9,24 +9,24 @@ class Species_Service:
         self.repo = SpeciesRepository(database.get_session())
         self.keylist = [[column.name,column.type] for column in inspect(Species).c]
 
-    def add(self):
-        self.args = {}
+    # def add(self):
+    #     self.args = {}
 
-        for i in range(len(self.keylist)):
-            if self.keylist[i][0] == "id": ##Add any other things if they should be auto-incremeneted
-                continue
+    #     for i in range(len(self.keylist)):
+    #         if self.keylist[i][0] == "id": ##Add any other things if they should be auto-incremeneted
+    #             continue
 
-            keyname = self.keylist[i][0]
-            keytype = self.keylist[i][1]
+    #         keyname = self.keylist[i][0]
+    #         keytype = self.keylist[i][1]
 
-            for type in typedict().keys(): ##Add types to dictionairies.py if your type is not found
-                if isinstance(keytype, type):
-                    self.input = typedict()[type](input(f'{keyname}: '))
+    #         for type in typedict().keys(): ##Add types to dictionairies.py if your type is not found
+    #             if isinstance(keytype, type):
+    #                 self.input = typedict()[type](input(f'{keyname}: '))
                     
-            self.args[keyname] = self.input
+    #         self.args[keyname] = self.input
         
-        self.repo.add(**self.args)
+    #     self.repo.add(**self.args)
 
-    def delete(self, **kwargs):
-        name = input("Which species would you like to delete?: ")
-        self.repo.delete(**self.args)
+    # def delete(self, **kwargs):
+    #     name = input("Which species would you like to delete?: ")
+    #     self.repo.delete(**self.args)

1534219ea542d7ac0be5d6b973866f5912470ab8
Arjan-van-Denzen <arjan@vandenzen.nl>
2024-07-29 13:39:47 +0200

    hallo

--- src/data_access/repositories/species_repo.py
+++ src/data_access/repositories/species_repo.py
@@ -5,14 +5,27 @@ class SpeciesRepository:
     def __init__(self, db_session):
         self.db_session = db_session
 
-    def add(self, **args):
-        new_species = Species(**args)
-        self.db_session.add(new_species)
+    def add(self, species):
+        self.db_session.add(species)
         self.db_session.commit()
-        field_dict = {**args}
-        print('A species was added to the database with the following fields:')
-        print(json.dumps(field_dict, indent = 4))
-        return new_species
     
-    def delete(self, **args):
-        pass
+    def get_all(self):
+        return self.db_session.query(Species).all()
+    
+    def get_all_active(self):
+        return self.db_session.query(Species).filter_by(active=True)
+    
+    def get_by_id(self, species_id):
+        return self.db_session.get(Species, species_id)
+    
+    def update(self, species_id, **kwargs):
+        species = SpeciesRepository.get_by_id(species_id)
+        for key, value in kwargs.items():
+            if hasattr(species, key):
+                setattr(species, key, value)
+            else:
+                print(f'Species does not have a field {key}.')
+        self.db_session.commit()
+    
+    def delete(self, species_id):
+        self.db_session.get(Species, species_id).active = False
--- src/data_access/services/species_service.py
+++ src/data_access/services/species_service.py
@@ -1,32 +1,71 @@
 from src.data_access.repositories.species_repo import SpeciesRepository
 from src.models.species import Species
+from src.models.landscape import Landscape
 from src.data_access.database import database
 from src.data_access.dictionaries import typedict
 from sqlalchemy.inspection import inspect
+import json
 
-class Species_Service:
+class SpeciesService:
     def __init__(self):
         self.repo = SpeciesRepository(database.get_session())
         self.keylist = [[column.name,column.type] for column in inspect(Species).c]
+
+    def get_all_active(self):
+        all_species = self.repo.get_all_active()
+        print("Species currently in your zoo:")
+        for species in all_species:
+            print(json.dumps(species.to_dict()))
+
 
     def add(self):
         self.args = {}
 
         for i in range(len(self.keylist)):
-            if self.keylist[i][0] == "id": ##Add any other things if they should be auto-incremeneted
+            if self.keylist[i][0] == "id": #Add any other things if they should be auto-incremeneted
                 continue
 
             keyname = self.keylist[i][0]
             keytype = self.keylist[i][1]
-
-            for type in typedict().keys(): ##Add types to dictionairies.py if your type is not found
-                if isinstance(keytype, type):
-                    self.input = typedict()[type](input(f'{keyname}: '))
+                
+            for type_i in typedict().keys(): #Add types to dictionairies.py if your type is not found
+                if isinstance(keytype, type_i):
+                    if type(keytype).__name__ == "Enum":
+                        if keyname == 'landscape':
+                            print('Which landscape does your species prefer? Choose from:')
+                            for name in Landscape.__members__:
+                                print(name)
+                            while True:
+                                testinput = typedict()[type_i](input(f'{keyname}: ')).upper()
+                                if testinput in Landscape.__members__:
+                                    self.input = testinput
+                                    break
+                                else:
+                                    print("Try again")
+                        # add other enum classes in if-statements if necessary
+                    else:
+                        self.input = typedict()[type_i](input(f'{keyname}: '))
                     
             self.args[keyname] = self.input
-        
-        self.repo.add(**self.args)
+        species = Species(**self.args)
+        try:
+            self.repo.add(species)
+            print(f'The following species was added to the database:\n{json.dumps(self.args, indent = 4)}')
+        except:
+            print("Something went wrong when you wanted to add the species to the database.")
 
-    def delete(self, **kwargs):
-        name = input("Which species would you like to delete?: ")
-        self.repo.delete(**self.args)
+    def update_species(self):
+        species = input("Which species would you like to update? Please choose from the list below.")
+        print(self.repo.get_all_active)
+        species_id = int(input('I choose to update the species with id: '))
+        update_dict = {}
+        keys = list(input('I want to update these fields (separate by comma): '))
+        for key in keys:
+            pass # use the getinputdict here!!!
+
+
+    def delete(self):
+        name = input("Which species would you like to delete? Please choose from the list below.")
+        print(self.repo.get_all_active)
+        species_id = int(input('I choose to delete the species with id: '))
+        self.repo.delete(species_id)
